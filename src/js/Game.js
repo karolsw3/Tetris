126da958e5379313ae41eae2f93b6cd807025550
Karol Świerczek <karol.sw3@gmail.com>
2018-03-28 23:43:58 +0200

    Sketch basic game functions

--- src/js/Game.js
+++ src/js/Game.js
@@ -1,3 +1,4 @@
+import Block from './Block'
 /**
  * Main class responsible for everything what is going on in the game
  * @constructor Game
@@ -7,6 +8,7 @@ class Game {
     this.View = {}
     this.canvas = document.getElementById('canvas')
     this.ctx = this.canvas.getContext('2d')
+    this.blocks = []
     this.board = []
     this.paused = false
     this.score = 0
@@ -18,19 +20,36 @@ class Game {
   init (sizeX, sizeY) {
     // Fill the board with empty tiles
     this.board = Array(...Array(sizeX)).map(() => Array(sizeY))
+    this.createBlock(Math.round(sizeX / 2), Math.round(sizeY / 2))
+    this.startGameInterval(800)
   }
 
   startGameInterval (frameTime) {
     setInterval(() => {
-      
+      this.moveBlocksDown()
     }, frameTime)
+  }
+
+  createBlock (x, y) {
+    this.blocks.push(new Block(x, y))
+  }
+
+  /**
+   * Moves all floating blocks one row down (Acts like a gravity)
+   */
+  moveBlocksDown () {
+    this.blocks.map(block => {
+      if (block.y > 0) { // TODO Check collisions between blocks
+        block.y -= 1
+      }
+    })
   }
 
   /**
    * Restart the game
    */
   restart () {
-
+    this.blocks = []
   }
 
   /**
@@ -47,6 +66,9 @@ class Game {
    */
   keyDown (event) {
     let keyCode = event.keyCode
+    if (keyCode === 83 || keyCode === 40) { // s OR down arrow
+      this.moveBlocksDown()
+    }
   }
 
   /**
