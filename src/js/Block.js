5e152f084b7a8763abedc424e3510e27c2a256e2
Ph0enixKM <pkaras.it@gmail.com>
2018-03-29 12:15:52 +0200

    Block class update

--- src/js/Block.js
+++ src/js/Block.js
@@ -1,13 +1,22 @@
 /**
  * (Paweł)
  * Block game element schema
- */
+*/
 class Block {
   constructor (param) {
     // Generate Locations
-    this.x = param.loc.x || function () { console.warn("Location x isn't defined"); return undefined }
-    this.y = param.loc.y || function () { console.warn("Location y isn't defined"); return undefined }
-
+    this.x = (
+      param.loc.x || (function () {
+        console.warn("Location x isn't defined")
+        return undefined
+      })()
+    )
+    this.x = (
+      param.loc.y || (function () {
+        console.warn("Location y isn't defined")
+        return undefined
+      })()
+    )
     // Generate number from range ( 0 - 6 ) - 7 elements
     this.type = Math.round(Math.random() * 6)
   }
